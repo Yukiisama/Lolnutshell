5a83a642d8e7bb29d6519c1bfe2e46851e0a8481
Alexis Perignon <aperigno@emi.u-bordeaux1.fr>
2020-02-24 18:32:37 +0100

    [Lol.Model.Dto] Add Match DTO.

---
+++ Domain/Lol/Model/Dto/MatchDto.py
@@ -0,0 +1,59 @@
+from Domain.Lol.Model.Dto.ParticipantDto           import ParticipantDto
+from Domain.Lol.Model.Dto.TeamStatsDto             import TeamStatsDto
+from Domain.Lol.Model.Dto.ParticipantIdentitiesDto import ParticipantIdentitiesDto
+
+class MatchDto():
+
+	def __init__(self, apiData):
+		self.seasonId              = apiData.get('seasonId', None) 
+		self.queueId               = apiData.get('queueId', None)
+		self.gameId                = apiData.get('gameId', None)
+		
+		self.participantIdentities = ([ParticipantIdentitiesDto(apiData['participantIdentities'][i])
+                                    for i in range(len(apiData['participantIdentities']))]
+		                            if 'participantIdentities' in apiData.keys() else None)
+		
+		self.gameVersion           = apiData.get('gameVersion', None)    
+		self.platformId            = apiData.get('platformId', None)    
+		self.gameMode              = apiData.get('gameMode', None)    
+		self.mapId                 = apiData.get('mapId', None)    
+		self.gameType              = apiData.get('gameType', None)
+			
+		self.teams                 = ([TeamStatsDto(apiData['teams'][i]) for i in range(len(apiData['teams']))]
+		                           if 'teams' in apiData.keys() else None)
+			
+		self.participants          = ([ParticipantDto(apiData['participants'][i])
+                                   for i in range(len(apiData['participants']))]
+		                           if 'participants' in apiData.keys() else None)
+		
+		self.gameDuration          = apiData.get('gameDuration', None)
+		self.gameCreation          = apiData.get('gameCreation', None)
+	
+	def print(self, participant = None):
+		print("Season ID:", self.seasonId)
+		print("Queue ID:" , self.queueId)
+		print("Game ID:"  , self.gameId)
+		print("ParticipantIdentities:")
+		
+		for p in self.participantIdentities:
+		    p.print()
+		    print()
+		i = 0
+		
+		for team in self.teams : 
+		    i+=1
+		    print("** Team  n° {} **".format(i))
+		    team.print()
+			
+		print("Game Version:", self.gameVersion)
+		print("Platform ID:" , self.platformId)
+		print("Game Mode:"   , self.gameMode)
+		print("Map ID:"      , self.mapId)
+		print("Game Type:"   , self.gameType)
+		
+		if participant :
+		    for p in self.participants: p.print()
+			
+		print("gameDuration:", self.gameDuration)
+		print("gameCreation:", self.gameCreation)
+		
